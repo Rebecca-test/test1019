e4c05e0e662f628b89aedf2cb450a1555245b8ba
Jenny Xu <v-mingx@microsoft.com>
2020-10-16 06:45:33 +0000

    789

--- ConsoleApp1/Program - Copy.cs
+++ ConsoleApp1/Program - Copy.cs
@@ -15,8 +15,6 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
         }
     }
 }
